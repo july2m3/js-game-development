3da199627210cc73bfb478c40208b00853aa7566
Julio <july2m3@gmail.com>
2020-05-20 12:54:19 -0500

    Updates

---
+++ precache-manifest.4c54f6129f528dcf38b4891a4de5eb1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78af0fb815072e89182be7399bef2ac",
+    "url": "/js-game-development/index.html"
+  },
+  {
+    "revision": "c0ac22a1b5550cb6fd90",
+    "url": "/js-game-development/static/css/main.be8bba9d.chunk.css"
+  },
+  {
+    "revision": "813878f40f08ea3f8477",
+    "url": "/js-game-development/static/js/2.baeb9f77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-game-development/static/js/2.baeb9f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ac22a1b5550cb6fd90",
+    "url": "/js-game-development/static/js/main.637f66cf.chunk.js"
+  },
+  {
+    "revision": "f5e3bbc3204a8ba79f86",
+    "url": "/js-game-development/static/js/runtime-main.bfc94713.js"
+  }
+]);
